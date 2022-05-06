544815c8e92ddc064847b9dddfb987cf4b4a433c
vinnyvilasboa <vinnycesca@gmail.com>
2022-05-06 10:57:09 -0400

    Up to ch4: EJS fully functional

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = new express()
+const path = require('path');
+const ejs = require('ejs')
+
+
+app.set('view engine', 'ejs')
+/*With the above code we tell EXPRESS to use EJS as our templating engine, that any file ending in .ejs should be rendered with the EJS package*/
+app.use(express.static('public'))
+app.listen(4000, ()=>{
+    console.log('App running on port 4000')
+})
+
+app.get('/', (req, res) =>{
+    // res.sendFile(path.resolve(__dirname,'pages/index.html'))
+    res.render('index')
+ 
+})
+
+app.get('/about', (req, res) =>{
+    // res.sendFile(path.resolve(__dirname,'pages/about.html'))
+    res.render('about')
+})
+
+app.get('/contact', (req, res) =>{
+    // res.sendFile(path.resolve(__dirname,'pages/contact.html'))
+    res.render('contact')
+})
+
+app.get('/post', (req, res) =>{
+    // res.sendFile(path.resolve(__dirname,'pages/post.html'))
+    res.render('post')
+
+})
