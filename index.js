d25ac1bf1f3bcca3da1fc7e1f7fce6dd7b87153a
vinnyvilasboa <vinnycesca@gmail.com>
2022-05-10 14:29:58 -0400

    POST: blog creation and index display working

--- index.js
+++ index.js
@@ -3,38 +3,57 @@ const app = new express()
 const path = require('path');
 const ejs = require('ejs')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const BlogPost = require('./models/BlogPost.js')
+mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true })
 
-mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser: true})
-
+/*With the below code we tell EXPRESS to use EJS as our templating engine, that any file ending in .ejs should be rendered with the EJS package*/
 app.set('view engine', 'ejs')
-/*With the above code we tell EXPRESS to use EJS as our templating engine, that any file ending in .ejs should be rendered with the EJS package*/
+
+//////////////MIDDLEWARE//////////////////
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
-app.listen(4000, ()=>{
-    console.log('App running on port 4000')
+//////////////MIDDLEWARE//////////////////
+//////////////////////////////////////////
+
+app.get('/', async (req, res) => {
+    const blogposts = await BlogPost.find({})
+    console.log(blogposts)
+    res.render('index', {
+        //whenever the key:value pair are the same we can shorten it to just one word
+        blogposts
+    })
 })
 
-app.get('/', (req, res) =>{
-    // res.sendFile(path.resolve(__dirname,'pages/index.html'))
-    res.render('index')
- 
-})
-
-app.get('/about', (req, res) =>{
+app.get('/about', (req, res) => {
     // res.sendFile(path.resolve(__dirname,'pages/about.html'))
     res.render('about')
 })
 
-app.get('/contact', (req, res) =>{
+app.get('/contact', (req, res) => {
     // res.sendFile(path.resolve(__dirname,'pages/contact.html'))
     res.render('contact')
 })
 
-app.get('/post', (req, res) =>{
+app.get('/post', (req, res) => {
     // res.sendFile(path.resolve(__dirname,'pages/post.html'))
     res.render('post')
 
 })
 
-app.get('/post/new',(req, res) => {
+app.get('/post/new', (req, res) => {
     res.render('create')
+})
+
+app.post('/posts/store', async (req, res) => {
+    ///////////model aka collection creates a new document with browser database///////////
+    ///////////we provide the req.body as the first argument and a callback function as the 2nd argument which is called when the create completes///////////
+   await BlogPost.create(req.body)
+        res.redirect('/')
+})
+
+
+app.listen(4000, () => {
+    console.log('App running on port 4000')
 })
